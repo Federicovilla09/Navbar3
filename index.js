3939310d2b80d315f563c81fcb400e9c2f0feba0
Federicovilla09 <federubents@gmail.com>
2022-07-26 10:07:25 -0300

    Se le agrego js

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu_visible");
+
+  if (navMenu.classList.contains("nav-menu_visible")) {
+    navToggle.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    navToggle.setAttribute("aria-label", "Abrir menú")
+  }
+
 });
